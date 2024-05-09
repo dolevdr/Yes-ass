61670549a9cda50682e13930ad8d9d0f196f6f50
dolevdr <73753487+dolevdr@users.noreply.github.com>
2024-05-09 18:32:11 +0300

    finish second part

--- classes/vodOrders.js
+++ classes/vodOrders.js
@@ -5,46 +5,112 @@ class VodOrders {
 
   splittedRecords(order) {
     const splitted = order.split(",");
-    const [userId, date, _, __, price] = splitted;
+    const [userId, date, genere, __, price] = splitted;
     const dayMonth = date.substring(0, 7);
-    return { userId, dayMonth, price };
+    return { userId, dayMonth, price, genere };
   }
 
+  // Part 1
+  // createTotalCustomerUsage() {
+  //   const dict = {};
+  //   for (let order of this.orders) {
+  //     const { userId, dayMonth, price } = this.splittedRecords(order);
+  //     const key = userId + "," + dayMonth;
+  //     if (key in dict) {
+  //       dict[key] = dict[key].price + +price;
+  //     } else {
+  //       dict[key] = price;
+  //     }
+  //   }
+  //   console.log("Customer,Month,TotalPrice");
+  //   for (let key in dict) {
+  //     const [userId, dayMonth] = key.split(",");
+  //     console.log(`${userId},${dayMonth},${isNaN(dict[key]) ? 0 : dict[key]}`);
+  //   }
+  // }
+
+  // Part 2
   createTotalCustomerUsage() {
     const dict = {};
     for (let order of this.orders) {
-      const { userId, dayMonth, price } = this.splittedRecords(order);
+      const { userId, dayMonth, price, genere } = this.splittedRecords(order);
       const key = userId + "," + dayMonth;
       if (key in dict) {
-        dict[key] = dict[key].price + +price;
+        dict[key] = {
+          price: dict[key].price + +price,
+          genere: {
+            ...dict[key].genere,
+            [genere]: (dict[key].genere[genere] || 0) + 1,
+          },
+        };
       } else {
-        dict[key] = price;
+        dict[key] = { price: +price, genere: { [genere]: 1 } };
       }
     }
-    console.log("Customer,Month,TotalPrice");
+    console.log("Customer,Month,TotalPrice,Discount");
     for (let key in dict) {
       const [userId, dayMonth] = key.split(",");
-      console.log(`${userId},${dayMonth},${isNaN(dict[key]) ? 0 : dict[key]}`);
+      console.log(
+        `${userId},${dayMonth},${
+          isNaN(dict[key].price) ? 0 : Math.round(dict[key].price)
+        },${
+          Object.keys(dict[key].genere).some(
+            (gen) => dict[key].genere[gen] >= 3
+          )
+            ? Math.round(0.25 * dict[key].price)
+            : 0
+        }`
+      );
     }
   }
+
+  // Part 1
+  // createSummaryReport() {
+  //   const dict = {};
+  //   for (let order of this.orders) {
+  //     const { dayMonth, price } = this.splittedRecords(order);
+  //     if (dayMonth in dict) {
+  //       dict[dayMonth] = {
+  //         amount: dict[dayMonth].amount++,
+  //         price: dict[dayMonth].price + +price,
+  //       };
+  //     } else {
+  //       dict[dayMonth] = { amount: 1, price: +price };
+  //     }
+  //   }
+  //   console.log("Month,TotalPrice,AmountOfOrders");
+  //   for (let key in dict) {
+  //     console.log(
+  //       `${key},${Math.round(dict[key].price) ?? 0},${dict[key].amount}`
+  //     );
+  //   }
+  // }
 
   createSummaryReport() {
     const dict = {};
     for (let order of this.orders) {
-      const { dayMonth, price } = this.splittedRecords(order);
+      const { dayMonth, price, genere } = this.splittedRecords(order);
       if (dayMonth in dict) {
         dict[dayMonth] = {
           amount: dict[dayMonth].amount++,
           price: dict[dayMonth].price + +price,
+          genere: {
+            ...dict[dayMonth].genere,
+            [genere]: (dict[dayMonth].genere[genere] || 0) + 1,
+          },
         };
       } else {
-        dict[dayMonth] = { amount: 1, price: +price };
+        dict[dayMonth] = { price: +price, genere: { [genere]: 1 }, amount: 1 };
       }
     }
-    console.log("Month,TotalPrice,AmountOfOrders");
+    console.log("Month,TotalPrice,AmountOfOrders,DiscountAmount");
     for (let key in dict) {
       console.log(
-        `${key},${Math.round(dict[key].price) ?? 0},${dict[key].amount}`
+        `${key},${Math.round(dict[key].price) ?? 0},${dict[key].amount}${
+          Object.keys(dict[key].genere).filter(
+            (gen) => dict[key].genere[gen] >= 3
+          ).length
+        }`
       );
     }
   }
@@ -59,8 +125,8 @@ class VodOrders {
 //   "0218354,2024-04-01 16:51:23,DRAMA,137227,19.90",
 //   "0238385,2024-05-01 17:54:23,NEWS,96737227,19.90",
 //   "0238322,2024-04-01 17:54:23,REALITY,96737227,29.90",
-//   "0238383,2024-05-01 18:54:23,COMEDY,5737227,00.90",
-//   "0238383,2024-05-01 17:54:23,ACTION,137227,19.90",
+//   "0238383,2024-04-01 18:54:23,COMEDY,5737227,00.90",
+//   "0238383,2024-04-01 17:54:23,COMEDY,137227,19.90",
 // ];
 
 // const vodOrders = new VodOrders(orders);
